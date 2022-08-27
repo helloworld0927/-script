456863e77f28456beb3d0a9ee5dd42dfa27f5b5c
Jacky W <wong.yiu.long@gmail.com>
2022-08-27 15:23:51 +0800

    Update sc2tc.js

--- sc2tc.js
+++ sc2tc.js
@@ -10,7 +10,6 @@
 // @description        任意轉換網頁中的簡體中文與繁體中文（默認簡體→繁體）
 // @description:zh-TW  任意轉換網頁中的簡體中文與繁體中文（默認簡體→繁體）
 // @description:zh-CN  任意转换网页中的简体中文与繁体中文（默认繁体→简体）
-// @description:en     Auto Switch Traditional Chinese and Simplified Chinese
 // @include      *
 // @exclude           *://*.github.com/*
 // @exclude           *://*.github.com/*
